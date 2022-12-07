5107d746bc69b8caf82cb0deda18438182a9e739
gdumfi <44841897+gdumfi@users.noreply.github.com>
2022-12-08 02:17:41 +0300

    pochemuto fuckup

---
+++ lab6.2 правильное/lab6.2 правильное/lab6.2 правильное.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <deque>
+#include <string>
+using namespace std;
+
+
+bool isstringpalidrom(deque <char> dec)
+{
+    bool flag = true;
+    while (not dec.empty())
+    {
+        if (dec.front() != dec.back());
+        flag  = false;
+        break;
+    }
+    return flag;
+}
+
+int main()
+{
+
+    string s = ""; //vvod
+    cin >> s;
+
+    deque <char> dec; // zapolnenie deka
+    for (char chrtr : s)
+    {
+        switch (chrtr)
+        {
+        case(true):
+        {
+            dec.push_back(chrtr);
+            break;
+        }
+        }
+    }
+
+    cout << s << " " << (isstringpalidrom(dec) ? "yes\n" : "no\n");
+}
