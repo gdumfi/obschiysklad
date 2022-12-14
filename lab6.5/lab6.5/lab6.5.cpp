0aaf9bdbc12bad0e4484b3a5fc3a3ac057ad5a02
gdumfi <44841897+gdumfi@users.noreply.github.com>
2022-12-14 17:04:44 +0300

    Add files via upload

---
+++ lab6.5/lab6.5/lab6.5.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string word;
+    map<string, int> counter;
+    double cnt=0;
+    while (cin >> word)
+    {
+        ++cnt;
+        counter[word]++;
+    }
+
+    map <string, int> ::iterator iter;
+
+    for (iter = counter.begin(); iter != counter.end(); ++iter)
+        cout << iter->first << ':' << iter->second << "    " << ((iter->second)/cnt)*100 << "%" << endl;
+
+    return 0;
+}
+
+/*
+import random
+
+
+with open('rendw', 'r') as file:
+    words = file.readlines()
+    words = [s.strip("\n") for s in words]
+    for k in range(100):
+        print(random.choice(words), end=" ")
+
+*/
